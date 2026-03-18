a2bed3763073241dd025054116d9e4ee807f0fd1
Alexandra Miresan <amiresan@gmail.com>
2026-03-18 12:34:46 +0200

    Added ConfigHelper
    Added ConfigHelper for accesing the connection string easier.

---
+++ BankingAppTeamB/BankingAppTeamB/BankingAppTeamB/Configuration/ConfigHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace BankingAppTeamB.Configuration;
+
+public static class ConfigHelper
+{
+    public static string GetConnectionString()
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        return config.GetConnectionString("BankingApp");
+    }
+}
