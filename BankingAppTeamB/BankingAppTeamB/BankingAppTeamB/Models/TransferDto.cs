deab8756e730ee7606485bfdf2a3550034a4bd3d
RobertPamfiloiu <pamfiloiurobi@yahoo.com>
2026-03-19 17:12:14 +0200

    Small tweaks
    Added default empty string for string types

--- BankingAppTeamB/BankingAppTeamB/BankingAppTeamB/Models/TransferDto.cs
+++ BankingAppTeamB/BankingAppTeamB/BankingAppTeamB/Models/TransferDto.cs
@@ -4,10 +4,10 @@ namespace BankingAppTeamB.Models
     {
         public int UserId { get; set; }
         public int SourceAccountId { get; set; }
-        public string RecipientName { get; set; }
-        public string RecipientIBAN { get; set; }
+        public string RecipientName { get; set; } = string.Empty;
+        public string RecipientIBAN { get; set; } = string.Empty;
         public decimal Amount { get; set; }
-        public string Currency { get; set; }
+        public string Currency { get; set; } = string.Empty;
         public string? Reference { get; set; }
         public string? TwoFAToken { get; set; }
     }
