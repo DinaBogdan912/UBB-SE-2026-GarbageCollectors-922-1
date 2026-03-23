4ddcdbc93520d4b1c9278fa8e7df918d1f094d86
MaximAndrei20 <andreimaxim2005@gmail.com>
2026-03-23 17:27:35 +0200

    Add value converters and register in App.xaml

---
+++ BankingAppTeamB/BankingAppTeamB/BankingAppTeamB/Converters/DecimalFormatConverter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace BankingAppTeamB.Converters
+{
+    public class DecimalFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is decimal d)
+            {
+                string symbol = parameter as string ?? "";
+                return string.IsNullOrEmpty(symbol) ? d.ToString("F2") : $"{symbol}{d:F2}";
+            }
+            return "0.00";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+            => throw new NotImplementedException();
+    }
+}
